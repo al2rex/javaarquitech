745019ee7cf6ca966997a551f9cd56aca4184df4
al2rex <arnaldo.castilla@gmail.com>
2020-12-02 22:03:21 -0500

    first commit

---
+++ src/main/java/co/jhasa/lab/app/JavaarchitechLabApplication.java
@@ -0,0 +1,13 @@
+package co.jhasa.lab.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaarchitechLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaarchitechLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/jhasa/lab/app/JavaarchitechLabApplicationTests.java
@@ -0,0 +1,13 @@
+package co.jhasa.lab.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaarchitechLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
